d4253e0406b5f96eb1dd6213c4a3c5ab5e8122ee
Stego <82957001+stegoioa@users.noreply.github.com>
2023-04-27 11:49:56 +0200

    Update image_segmentation_and_stich.py

--- image_processing_tools/image_segmentation_and_stich.py
+++ image_processing_tools/image_segmentation_and_stich.py
@@ -1,16 +1,11 @@
 
 #Use the Split_Image Section for Segmenting Images
 
-
-
 !pip install split-image
 
 !split-image /content/drive/MyDrive/projects/diploma/buildingheights/22.jpg 10 10
 
-
 #Use the Stich_Image Section for Stich Images Back Together
-
-
 
 git clone https://github.com/adamancer/stitch2d
 python -m pip install -U scikit-image
